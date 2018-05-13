27f62efe8587354ca68a4e615514d33a7cbf8d02
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-13 23:40:34 +1000

    Add starting method

--- src/CrawlGui.java
+++ src/CrawlGui.java
@@ -1,5 +1,25 @@
-public class CrawlGui {
+
+public class CrawlGui extends Application {
+    private static Room startingRoom;
+
     public static void main(String args[]) {
+        if (args.length != 1) {
+            System.out.println("Usage: java CrawlGui mapname");
+            System.exit(1);
+        }
+        if (MapIO.loadMap(args[0]) != null) {
+            startingRoom = (Room) MapIO.loadMap(args[0])[1];
+
+        } else if (MapIO.deserializeMap(args[0]) != null) {
+            startingRoom = MapIO.deserializeMap(args[0]);
+        }
+        else {
+            System.out.println("Unable to load file");
+            System.exit(2);
+        }
+        launch(args);
+    }
+    public void start(Stage stage) {
 
     }
 }
