492c4a4cbf6f0ca71a8bf96c538f7258eb93ab20
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-16 00:01:16 +1000

    Add initial message

--- src/CrawlGui.java
+++ src/CrawlGui.java
@@ -58,6 +58,7 @@ public class CrawlGui extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        log("You find your self in " + currentRoom.getDescription());
     }
 
     private void generateButtons() {
