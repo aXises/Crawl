b2c331876886b73957de64e385aaede1869d314c
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-27 17:05:52 +1000

    Remove space between operators

--- src/Cartographer.java
+++ src/Cartographer.java
@@ -1,6 +1,5 @@
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 
 public class Cartographer extends Canvas {
     // The graphicsContext object containing the canvas
@@ -28,8 +27,8 @@ public class Cartographer extends Canvas {
      */
     public void update() {
         bm.walk();
-        setWidth((bm.xMax + abs(bm.xMin) + 2) * length);
-        setHeight((bm.yMax + abs(bm.yMin) + 2) * length);
+        setWidth((bm.xMax+abs(bm.xMin)+2)*length);
+        setHeight((bm.yMax+abs(bm.yMin)+2)*length);
         gc.clearRect(0, 0, getWidth(), getHeight());
         for (Room key: bm.coords.keySet()) {
             drawRoom(bm.coords.get(key).x, bm.coords.get(key).y, key);
@@ -45,35 +44,35 @@ public class Cartographer extends Canvas {
     private void drawRoom(int x, int y, Room room) {
         // Calculate mid point of one room with respect to the global mid point
         // of the canvas
-        Double xMid = getMid()[0] - (length / 2) + (x * length);
-        Double yMid = getMid()[1] - (length / 2) + (y * length);
+        Double xMid = getMid()[0]-(length/2)+(x*length);
+        Double yMid = getMid()[1]-(length/2)+(y*length);
         // Calculate offset based on the amount and coordinates of the rooms
         // in a map
-        Double xOffset = (abs(bm.xMin) - bm.xMax) * length / 2;
-        Double yOffset = (abs(bm.yMin) - bm.yMax) * length / 2;
+        Double xOffset = (abs(bm.xMin)-bm.xMax)*(length/2);
+        Double yOffset = (abs(bm.yMin)-bm.yMax)*(length/2);
         // Get the final coordinates to draw the room at
-        Double xPos = xMid + xOffset;
-        Double yPos = yMid + yOffset;
+        Double xPos = xMid+xOffset;
+        Double yPos = yMid+yOffset;
         // Draws the box
         gc.strokeRect(xPos, yPos, length, length);
         // Adds exits lines
         for (String exit : room.getExits().keySet()) {
             switch(exit) {
                 case "North":
-                    gc.strokeLine(xPos + (length / 2), yPos - 5,
-                            xPos + (length / 2), yPos);
+                    gc.strokeLine(xPos+(length/2), yPos-5,
+                            xPos+(length/2), yPos);
                     break;
                 case "South":
-                    gc.strokeLine(xPos + (length / 2), yPos + length, xPos +
-                            (length / 2), yPos + length + 5);
+                    gc.strokeLine(xPos+(length/2), yPos+length,
+                            xPos+(length/2), yPos+length+5);
                     break;
                 case "East":
-                    gc.strokeLine(xPos + length, yPos + (length / 2), xPos +
-                            length + 5, yPos + (length / 2));
+                    gc.strokeLine(xPos+length, yPos+(length/2),
+                            xPos+length+5, yPos+(length/2));
                     break;
                 case "West":
-                    gc.strokeLine(xPos, yPos + (length / 2), xPos - 5, yPos +
-                            (length / 2));
+                    gc.strokeLine(xPos, yPos+(length/2), xPos-5,
+                            yPos+(length/2));
                     break;
                 default:
                     break;
@@ -82,13 +81,13 @@ public class Cartographer extends Canvas {
         // Adds Thing to the room
         for (Thing thing : room.getContents()) {
             if (thing instanceof Player) {
-                gc.fillText("@", xPos, yPos + length / 4);
+                gc.fillText("@", xPos, yPos+length/4);
             }
             else if (thing instanceof Treasure) {
-                gc.fillText("$", xPos + (length / 2), yPos + (length / 4));
+                gc.fillText("$", xPos+(length/2), yPos+(length/4));
             }
             else if (thing instanceof Critter) {
-                gc.fillText("M", xPos, yPos + (length / 1.5));
+                gc.fillText("M", xPos, yPos+(length/1.5));
             }
         }
     }
@@ -108,7 +107,7 @@ public class Cartographer extends Canvas {
      * @return The absolute value of the number
      */
     private double abs(int n) {
-        return n < 0 ? (-1 * n) : n;
+        return n<0 ? (-1*n) : n;
     }
 
 }
